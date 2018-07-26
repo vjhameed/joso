51b964eb4693b786d1572316ce15a89474f73fbb
hameed khan <vjhameed3@gmail.com>
2018-07-26 23:38:46 +0500

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-           // URL::ForceScheme('https');
+        URL::ForceScheme('https');
     }
 
     /**
